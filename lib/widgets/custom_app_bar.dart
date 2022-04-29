6f714b6f161a0ecb2a396c687516b0f40b2dc94f
Francis KOUAHO <kouahofrancis@gmail.com>
2022-04-29 13:16:44 +0200

    fixed appbar picture profile

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -55,22 +55,32 @@ class _CustomAppBarState extends State<CustomAppBar> {
       ),
       backgroundColor: LetsGoTheme.main,
       actions: <Widget>[
-        const IconButton(
-          icon: FaIcon(FontAwesomeIcons.solidBell, color: LetsGoTheme.white),
-          onPressed: null,
-        ),
-        GestureDetector(
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(user!.photoURL ??
-                "https://cdn.pixabay.com/photo/2016/04/22/04/57/graduation-1345143_1280.png"),
-          ),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProfilScreen()),
-            );
-          },
-        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const IconButton(
+              icon:
+                  FaIcon(FontAwesomeIcons.solidBell, color: LetsGoTheme.white),
+              onPressed: null,
+            ),
+            Container(
+                padding: const EdgeInsets.all(5),
+                child: GestureDetector(
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(user!.photoURL ??
+                        "https://cdn.pixabay.com/photo/2016/04/22/04/57/graduation-1345143_1280.png"),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ProfilScreen()),
+                    );
+                  },
+                ))
+          ],
+        )
       ],
     );
   }
