11129b74855aa4d4caa461a5cc1e9f0bb14c414b
jackytruongpro <jacky-t@hotmail.fr>
2022-05-04 02:38:04 +0200

    refonte cartes activités

--- lib/widgets/home/home_theme_section.dart
+++ lib/widgets/home/home_theme_section.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:letsgo/common/resume_word.dart';
 import 'package:letsgo/views/event/event_screen.dart';
 
 class HomeThemeSection extends StatelessWidget {
@@ -9,7 +10,6 @@ class HomeThemeSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final Stream<QuerySnapshot> _activitiesStream =
         FirebaseFirestore.instance.collection('activities').snapshots();
-
 
     return StreamBuilder<QuerySnapshot>(
         stream: _activitiesStream,
@@ -38,32 +38,114 @@ class HomeThemeSection extends StatelessWidget {
                 children: snapshot.data!.docs.map((DocumentSnapshot document) {
                   Map<String, dynamic> activity =
                       document.data()! as Map<String, dynamic>;
-                  return Card(
-                    child: ListTile(
-                      leading: Image.network(
-                        activity["image"] ??
-                            "https://www.elektroaktif.com.tr/assets/images/noimage.jpg",
-                        width: 80,
-                        height: 80,
+                  return Container(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 6, 0, 6),
+                      child: Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                        child: Padding(
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(13),
+                                child: Image.network(
+                                  activity["image"] ??
+                                      "https://www.elektroaktif.com.tr/assets/images/noimage.jpg",
+                                  width: 66,
+                                  height: 66,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      17, 0, 0, 0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Image.asset(
+                                            'assets/icons/DetailButton.png',
+                                            width: 17,
+                                            height: 17,
+                                            fit: BoxFit.cover,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsetsDirectional
+                                                .fromSTEB(7, 0, 0, 0),
+                                            child: Text(
+                                              activity['titleCategory'] ?? '',
+                                              style: const TextStyle(
+                                                color: Color(0xFF1D2429),
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsetsDirectional
+                                            .fromSTEB(0, 4, 0, 0),
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.max,
+                                          children: [
+                                            Text(
+                                              ResumeWord(
+                                                  activity['title'] ?? ''),
+                                              style: const TextStyle(
+                                                color: Color(0xFF57636C),
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w400,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
+                                child: Card(
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  elevation: 0,
+                                  child: InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => EventScreen(
+                                                  activity: activity)));
+                                    },
+                                    child: Image.asset(
+                                      "assets/icons/DetailButton.png",
+                                      width: 30,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      title: Text(activity['titleCategory'] ?? '',
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black)),
-                      subtitle: Text(activity['title'] ?? '',
-                          style: const TextStyle(color: Colors.black)),
-                      trailing: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>  EventScreen(
-                                    activity: activity
-                                  )));
-                        },
-                        child: Image.asset("assets/icons/DetailButton.png"),
-                      ),
-                      isThreeLine: true,
                     ),
                   );
                 }).toList(),
