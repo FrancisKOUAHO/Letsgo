737d70c1bb84cf82c229b5736837006fbdae5c89
Francis KOUAHO <kouahofrancis@gmail.com>
2022-05-16 15:38:29 +0200

    fixed

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import '../../theme/letsgo_theme.dart';
 import '../../widgets/custom_app_bar/custom_app_bar.dart';
 import '../../widgets/home/home_slider_section.dart';
-import '../../widgets/home/home_subtitle_section.dart';
 import '../../widgets/home/home_theme_section.dart';
 import '../../widgets/home/user_box_title_section.dart';
+import '../search/research.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -36,6 +36,34 @@ class _HomeState extends State<Home> {
                 height: 20,
               ),
               Container(
+                margin: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(10),
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: TextField(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Research()),
+                      );
+                    },
+                    decoration: InputDecoration(
+                      fillColor: LetsGoTheme.lightPurple,
+                      filled: true,
+                      contentPadding:
+                          const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                      suffixIcon: Icon(Icons.search, color: LetsGoTheme.main),
+                      hintText: "Rechercher des activité ...",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
                 padding: const EdgeInsets.only(left: 10),
                 child: Text(
                   "Suggestion pour toi",
@@ -48,6 +76,7 @@ class _HomeState extends State<Home> {
               const HomeSliderSection(),
               Container(
                 padding: const EdgeInsets.only(left: 10),
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                 child: Text(
                   "Les activités",
                   style: LetsGoTheme.subTitle,
